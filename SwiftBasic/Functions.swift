9fa3b7674ced045fbb089ad469591cdf4c76b20f
Ekaterina Kovanikova <kate9rina@gmail.com>
2026-02-11 16:13:01 +0000

    Add Swift files properly

---
+++ SwiftBasic/Functions.swift
@@ -0,0 +1,12 @@
+func runFunctions (){
+    let firstSurname = "Kate"
+    //firstName = "Dima"// let is constant
+    
+    var secondSurname = "Victoria"
+    secondSurname = "Elizabeth"
+    secondSurname = "Arthur"
+    
+    print (secondSurname)
+    print(firstSurname)
+}
+
---
+++ SwiftBasic/loop.swift
@@ -0,0 +1,7 @@
+//
+//  loop.swift
+//  SwiftBasic
+//
+//  Created by Ekaterina Kovanikova on 11/02/2026.
+//
+
